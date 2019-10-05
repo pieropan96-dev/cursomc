c30c2e85c66783bdd22844d86c4c023b8ced502b
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2019-10-05 15:46:17 -0300

    Projeto criado.

---
+++ src/main/java/com/pieropan/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.pieropan.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
